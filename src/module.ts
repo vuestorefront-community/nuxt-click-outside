a26667f999b87aa2f26820e2e4b92a3b69d6002f
Daniel Roe <daniel@roe.dev>
2022-06-28 14:29:38 +0100

    fix: remove compiler options

--- src/module.ts
+++ src/module.ts
@@ -6,19 +6,7 @@ export default defineNuxtModule({
   meta: {
     name: 'nuxt-click-outside',
   },
-  setup(_options, nuxt) {
-    nuxt.options.vue.compilerOptions.directiveTransforms =
-      nuxt.options.vue.compilerOptions.directiveTransforms || {};
-    nuxt.options.vue.compilerOptions.directiveTransforms['click-outside'] =
-      () => ({
-        props: [],
-        needRuntime: true,
-      });
-    nuxt.options.vue.compilerOptions.directiveTransforms.clickOutside = () => ({
-      props: [],
-      needRuntime: true,
-    });
-
+  setup() {
     const runtimeDir = fileURLToPath(new URL('runtime', import.meta.url));
 
     addPlugin(resolve(runtimeDir, 'plugin'));
